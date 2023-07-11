4e11db31c47e9747477252923d005a7d4cc9db37
changkim1 <zzankor@naver.com>
2023-07-11 17:58:12 +0900

    Home_calendar 추가

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,11 +3,22 @@ import Nav from "./Nav"
 import Search from "./Search"
 import home_styles from "../styles/Home.module.css"
 
+function Home_calendar() {
+	return (
+		<div className={home_styles.home_calendar_wrap}>
+			<div>
+				Home_calendar
+			</div>
+		</div>
+	)
+}
+
 export default function Home() {
   return (
 	<div className={home_styles.home_wrap}>
         <Nav />
         <Search />
+		<Home_calendar />
 	</div>
   )
 }
