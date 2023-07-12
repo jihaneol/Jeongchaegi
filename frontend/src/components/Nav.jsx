334820934819485721f2f3f20f2297d9ae1bd780
changkim1 <zzankor@naver.com>
2023-07-12 14:53:08 +0900

    (to_develop) Navbar css 적용(로고 동그랗게, div 정렬 등)

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
     <div className={nav_styles.nav_wrap}>
       <div className={nav_styles.nav_menu}>
         <Link to={"/"} className={nav_styles.nav_logo}></Link>
-        <Link to="/list">정책list </Link>
+        <Link to="/list" className={nav_styles.nav_list}>정책list </Link>
         <div>calendar</div>
       </div>
       <div>
