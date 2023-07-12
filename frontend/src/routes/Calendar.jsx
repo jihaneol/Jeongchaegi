221758fdcdeee2ef3f404aed36009147cbcb87e6
changkim1 <zzankor@naver.com>
2023-07-12 15:46:37 +0900

    style: Apply code formater

--- frontend/src/routes/Calendar.jsx
+++ frontend/src/routes/Calendar.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Calendar() {
-  return (
-	<h1>Calendar</h1>
-  )
+  return <h1>Calendar</h1>;
 }
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import Search from "../components/Search"
-import HotPost from '../components/HotPost'
-import home_styles from "../styles/Home.module.css"
-import Deadline from '../components/Deadline'
+import React from "react";
+import Search from "../components/Search";
+import HotPost from "../components/HotPost";
+import home_styles from "../styles/Home.module.css";
+import Deadline from "../components/Deadline";
 
 function HomeCalendar() {
-	return (
-		<div className={home_styles.home_calendar_wrap}>
-			<div className={home_styles.home_calendar_center}>
-				HomeCalendarCenter
-			</div>
-		</div>
-	)
+  return (
+    <div className={home_styles.home_calendar_wrap}>
+      <div className={home_styles.home_calendar_center}>HomeCalendarCenter</div>
+    </div>
+  );
 }
 
 export default function Home() {
   return (
-	<div className={home_styles.home_wrap}>
-        <Search />
-		<HomeCalendar />
-		<div className={home_styles.home_lower_wrap}>
-			<HotPost />
-			<Deadline />
-		</div>
-	</div>
-  )
+    <div className={home_styles.home_wrap}>
+      <Search />
+      <HomeCalendar />
+      <div className={home_styles.home_lower_wrap}>
+        <HotPost />
+        <Deadline />
+      </div>
+    </div>
+  );
 }
