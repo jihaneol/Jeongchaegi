9aafbcd23aad793693ae539a33e6c1d127b2116a
changkim1 <zzankor@naver.com>
2023-07-14 17:39:58 +0900

    feat: beautify Home.module.css file

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -5,7 +5,6 @@ import home_styles from "../styles/Home.module.css";
 import Deadline from "../components/Deadline";
 import HomeCalendar from "../components/HomeCalendar";
 
-
 export default function Home() {
   return (
     <div className={home_styles.home_wrap}>
