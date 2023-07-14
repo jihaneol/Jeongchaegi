776e9d66103d65b17d598b3d427e7d7e169055ed
changkim1 <zzankor@naver.com>
2023-07-14 17:30:35 +0900

    feat: homecalendar complete without animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from "./components/Nav";
+import Nav from "./routes/Nav";
 import Home from "./routes/Home";
 import List from "./routes/List";
 import Calendar from "./routes/Calendar";
 import SearchPage from "./routes/SearchPage";
-import Header from "./components/Header";
+import Header from "./routes/Header";
 
 function App() {
   return (
--- frontend/src/components/HomeCalendar.jsx
+++ frontend/src/components/HomeCalendar.jsx
@@ -3,14 +3,25 @@ import home_styles from "../styles/Home.module.css";
 import "../styles/HomeCalendar.css";
 import Calendar from "react-calendar";
 
-function ReactCalendar({position}) {
+function ReactCalendar({ position, month, year, day }) {
   const [pos, setPos] = useState("");
+  const [targetDate, setTargetDate] = useState(new Date());
+  const [targetDay, setTargetDay] = useState(day);
+
   useEffect(() => {
     setPos(position);
-  }, [position])
+  }, [position]);
+
+  useEffect(() => {
+    const newDate = new Date(year, month, targetDay);
+    setTargetDate(newDate);
+  }, [month, year, targetDay]);
+
   function calendarClick(v) {
-    console.log(v);
+    console.log(v.getDate());
+    setTargetDay(v.getDate());
   }
+
   return (
     <Calendar
       minDetail="month"
@@ -23,23 +34,43 @@ function ReactCalendar({position}) {
       formatDay={(locale, date) =>
         date.toLocaleString("en", { day: "numeric" })
       }
+      value={targetDate}
       onClickDay={calendarClick}
-	  className={`${home_styles.home_calendar_component} ${home_styles[pos]}`}
+      className={`${home_styles.home_calendar_component} ${home_styles[pos]} `}
     />
   );
 }
 
 export default function HomeCalendar() {
+  const [today, setToday] = useState(new Date());
+  const [month, setMonth] = useState(today.getMonth());
+  const year = today.getFullYear();
+  const day = today.getDate();
+
+  useEffect(() => {
+    setToday(new Date());
+  }, []);
+
+  function clickLeftMonth() {
+    setMonth(month - 1);
+  } 
+
+  function clickRightMonth() {
+    setMonth(month + 1);
+  }
+
   return (
     <div className={home_styles.home_calendar_wrap}>
-      <ReactCalendar
-        position="center"
-      />
-      <ReactCalendar
-        position="left"
-      />
-      <ReactCalendar
-        position="right"
+      <ReactCalendar position="center" month={month} year={year} day={day} />
+      <ReactCalendar position="left" month={month - 1} year={year} day={day} />
+      <button
+        onClick={clickLeftMonth}
+        className={`${home_styles.left} ${home_styles.home_calendar_component} ${home_styles.home_calendar_btn}`}
+        />
+      <ReactCalendar position="right" month={month + 1} year={year} day={day} />
+      <button
+        onClick={clickRightMonth}
+        className={`${home_styles.right} ${home_styles.home_calendar_component} ${home_styles.home_calendar_btn}`}
       />
     </div>
   );
