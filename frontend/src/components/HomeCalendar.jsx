3ef63b45f89333648f3adb6e225644762948cb9b
changkim1 <zzankor@naver.com>
2023-07-14 16:27:55 +0900

    refactor: remove ReactCalendar.jsx and move ReactCalendar component to HomeCalendar.jsx

--- frontend/src/components/HomeCalendar.jsx
+++ frontend/src/components/HomeCalendar.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
 import home_styles from "../styles/Home.module.css";
-import ReactCalendar from './ReactCalendar';
+import "../styles/HomeCalendar.css";
+import Calendar from "react-calendar";
+
+function ReactCalendar({position}) {
+  const [pos, setPos] = useState("");
+  useEffect(() => {
+    setPos(position);
+  }, [position])
+  function calendarClick(v) {
+    console.log(v);
+  }
+  return (
+    <Calendar
+      minDetail="month"
+      maxDetail="month"
+      nextLabel={null}
+      prevLabel={null}
+      next2Label={null}
+      prev2Label={null}
+      showNeighboringMonth={false}
+      formatDay={(locale, date) =>
+        date.toLocaleString("en", { day: "numeric" })
+      }
+      onClickDay={calendarClick}
+	  className={`${home_styles.home_calendar_component} ${home_styles[pos]}`}
+    />
+  );
+}
 
 export default function HomeCalendar() {
   return (
