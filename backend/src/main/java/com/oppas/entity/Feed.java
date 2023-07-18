2bbc2fc758acb9a8cd11fffa3661b19dacc8a7df
김성훈 <k_shun@naver.com>
2023-07-18 11:22:00 +0900

    fix: 엔티티 컬럼 속성 수정

--- backend/src/main/java/com/oppas/entity/Feed.java
+++ backend/src/main/java/com/oppas/entity/Feed.java
@@ -4,18 +4,21 @@ import lombok.Getter;
 
 import javax.persistence.*;
 
-@Entity(name = "feed")
+@Entity
 @Getter
 public class Feed {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feed_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     private String title;
+
     private String content;
+
 }
--- backend/src/main/java/com/oppas/entity/Member.java
+++ backend/src/main/java/com/oppas/entity/Member.java
@@ -8,16 +8,22 @@ import javax.persistence.*;
 @Entity
 @Getter
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String name;
+
     private int age;
+
     private String region;
+
     private String role;
-
-
 
 }
