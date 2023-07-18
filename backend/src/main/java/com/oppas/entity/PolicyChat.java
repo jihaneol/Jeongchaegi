1369e0d1bf27048f2ed2644e134c0ab66e0222ca
김성훈 <k_shun@naver.com>
2023-07-18 11:20:41 +0900

    feat: 정책 관련 채팅 엔티티 설계

---
+++ backend/src/main/java/com/oppas/entity/PolicyChat.java
@@ -0,0 +1,30 @@
+package com.oppas.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class PolicyChat {
+
+    @Id
+    @Column(name = "policy_chat_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "policy_id", nullable = false)
+    private Policy policy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime time;
+
+    private String content;
+
+}
