c49a95bcc7e8d7c0fd57869a80622b19179ae9e9
김성훈 <k_shun@naver.com>
2023-07-19 15:21:43 +0900

    feat: 정책 api 데이터 DTO 설계

---
+++ backend/src/main/java/com/oppas/dto/PolicyDTO.java
@@ -0,0 +1,40 @@
+package com.oppas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PolicyDTO {
+    private final int rnum;
+    private final String bizId;
+    private final String polyBizSjnm;
+    private final String polyItcnCn;
+    private final String sporCn;
+    private final String sporScvl;
+    private final String bizPrdCn;
+    private final String prdRpttSecd;
+    private final String rqutPrdCn;
+    private final String ageInfo;
+    private final String majrRqisCn;
+    private final String empmSttsCn;
+    private final String splzRlmRqisCn;
+    private final String accrRqisCn;
+    private final String prcpCn;
+    private final String aditRscn;
+    private final String prcpLmttTrgtCn;
+    private final String rqutProcCn;
+    private final String pstnPaprCn;
+    private final String jdgnPresCn;
+    private final String rqutUrla;
+    private final String rfcSiteUrla1;
+    private final String rfcSiteUrla2;
+    private final String mngtMson;
+    private final String mngtMrofCherCn;
+    private final String cherCtpcCn;
+    private final String cnsgNmor;
+    private final String tintCherCn;
+    private final String tintCherCtpcCn;
+    private final String etct;
+    private final String polyRlmCd;
+}
