629e2954e4db2b2b59cab2eccf258a27c29088ea
changkim1 <zzankor@naver.com>
2023-07-28 13:52:41 +0900

    feat: login Page, login Test

--- frontend/next/pages/login/index.jsx
+++ frontend/next/pages/login/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import styles from "../styles/Login.module.css";
+import styles from "../../styles/Login.module.css";
 import Image from "next/image";
-import Nav from "../components/Nav"
-import Login_test from "../components/Login_test"
+import Nav from "../../components/Nav"
+import Login_test from "../../components/Login_test"
+import Link from "next/link";
 
 function KakaoLoginOrSignup() {
   return (
@@ -10,7 +11,9 @@ function KakaoLoginOrSignup() {
 		<Nav />
 		<Login_test />
 		<div className={styles.content_container}>
-			<div className={styles.head_wrapper}>header</div>
+			<div className={styles.head_wrapper}>
+				<Image src="/Logo.PNG" width="200px" height="72px" alt="정채기 로고" />
+			</div>
 			<div className={styles.content_wrapper}>
 				<div className={styles.text_wrapper}>카카오 로그인으로 모든 기능 이용하기</div>
 				<div className={styles.kakao_wrapper}>
@@ -21,7 +24,9 @@ function KakaoLoginOrSignup() {
 			</div>
 			<div className={styles.signup_wrapper}>
 				계정이 없으신가요?
-				<a> 가입하기</a>
+				<Link href="/login/signup">
+					<a> 가입하기</a>
+				</Link>
 			</div>
 		</div>
 		{/* <div className={styles.footer_wrapper}>
