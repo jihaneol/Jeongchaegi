a6a7c58411aeed72f2ae1db1082dfe404976ff5b
shimshim <11goatmxj@naver.com>
2023-07-31 21:23:00 +0900

    [feat] jenkins

--- backend/src/test/java/com/oppas/service/PolicyServiceTest.java
+++ backend/src/test/java/com/oppas/service/PolicyServiceTest.java
@@ -1,21 +1,21 @@
-package com.oppas.service;
-
-import com.oppas.repository.PolicyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class PolicyServiceTest {
-
-    @Autowired
-    PolicyRepository policyRepository;
-
-    @Autowired
-    PolicyService policyService;
-
-
-}
+//package com.oppas.service;
+//
+//import com.oppas.repository.PolicyRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//@TestPropertySource(locations = "classpath:application-test.properties")
+//public class PolicyServiceTest {
+//
+//    @Autowired
+//    PolicyRepository policyRepository;
+//
+//    @Autowired
+//    PolicyService policyService;
+//
+//
+//}
