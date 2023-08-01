8aed654349335d4dd2cf45cfce9c4c5a7d827049
김창희 <zzankor@naver.com>
2023-08-01 17:02:25 +0900

    refactor: delete Login_test Component

--- frontend/next/components/PolicyListSort.jsx
+++ frontend/next/components/PolicyListSort.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import style from "../styles/PolicyList.module.css";
+import styles from "../styles/policylist.module.css";
 import Image from "next/image";
 
 export default function PolicyListSort() {
