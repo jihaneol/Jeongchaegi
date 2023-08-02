04f37285db43dcc43b38de20d294de518b37c390
changkim1 <zzankor@naver.com>
2023-08-02 10:25:11 +0900

    feat: domain name modify

--- backend/src/main/java/com/oppas/jwt/JwtService.java
+++ backend/src/main/java/com/oppas/jwt/JwtService.java
@@ -155,7 +155,7 @@ public class JwtService {
             Cookie cookie = new Cookie("at", accessToken);
             cookie.setMaxAge(60 * 2);
             cookie.setPath("/");
-            cookie.setDomain("localhost:3000");
+            cookie.setDomain("localhost:3000/");
             //        cookie.setHttpOnly(true);
             response.addCookie(cookie);
         }
@@ -172,7 +172,7 @@ public class JwtService {
             Cookie cookie = new Cookie("rt", refreshToken);
             cookie.setMaxAge(2 * 60);
             cookie.setPath("/");
-            cookie.setDomain("localhost:3000");
+            cookie.setDomain("localhost:3000/");
             //        cookie.setHttpOnly(true);
             response.addCookie(cookie);
 
