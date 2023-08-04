5882eac83d1b4da18dbc8e136c723c3eec91d21b
Inbeom Kim <dlsqja970210@naver.com>
2023-08-04 15:24:47 +0900

    feat: Create redux-persist

--- frontend/next/store/index.js
+++ frontend/next/store/index.js
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 
 import userReducer from "./user";
 
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 // redux 생성 (저장소)
 const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
 
 export default store;
