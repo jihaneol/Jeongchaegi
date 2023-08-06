b78cf590f0780e5efb0b9ac76a3e9df08e3cabb0
knavoid <k_shun@naver.com>
2023-08-06 09:42:35 +0900

    refactor: response를 dto로 변환

--- backend/src/main/java/com/oppas/controller/PolicyController.java
+++ backend/src/main/java/com/oppas/controller/PolicyController.java
@@ -1,11 +1,6 @@
 package com.oppas.controller;
 
-import com.oppas.dto.PolicyFilterDTO;
-import com.oppas.dto.PolicyScrapDTO;
-import com.oppas.entity.policy.Policy;
-import com.oppas.entity.policy.PolicyRegion;
-import com.oppas.entity.policy.PolicyScrap;
-import com.oppas.entity.policy.PolicyType;
+import com.oppas.dto.*;
 import com.oppas.service.PolicyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -23,35 +18,35 @@ public class PolicyController {
     private final PolicyService policyService;
 
     @GetMapping("/policies")
-    public ResponseEntity<Page<Policy>> getPolicies(@ModelAttribute PolicyFilterDTO filter, @RequestParam int pageIndex) throws Exception {
-        Page<Policy> policyList = policyService.getPolicies(filter, pageIndex);
-        return ResponseEntity.ok(policyList);
+    public ResponseEntity<Page<PolicySummaryDTO>> getPolicies(@ModelAttribute PolicyFilterDTO filter, @RequestParam int pageIndex) throws Exception {
+        Page<PolicySummaryDTO> policies = policyService.getPolicies(filter, pageIndex);
+        return ResponseEntity.ok(policies);
     }
 
     @GetMapping("/policies/{policyId}")
-    public ResponseEntity<Policy> getPolicy(@PathVariable Long policyId) throws Exception {
+    public ResponseEntity<PolicyDetailDTO> getPolicy(@PathVariable Long policyId) throws Exception {
         return ResponseEntity.ok(policyService.getPolicy(policyId));
     }
 
     @GetMapping("/policies/type")
-    public ResponseEntity<List<PolicyType>> getPolicyTypes() throws Exception {
+    public ResponseEntity<List<PolicyTypeDTO>> getPolicyTypes() throws Exception {
         return ResponseEntity.ok(policyService.getPolicyTypes());
     }
 
     @GetMapping("/policies/region")
-    public ResponseEntity<List<PolicyRegion>> getPolicyRegions() throws Exception {
+    public ResponseEntity<List<PolicyRegionDTO>> getPolicyRegions() throws Exception {
         return ResponseEntity.ok(policyService.getPolicyRegions());
     }
 
     @PostMapping("/policies/scrap")
-    public ResponseEntity<PolicyScrap> createPolicyScrap(@RequestBody PolicyScrapDTO policyScrapDTO) throws Exception {
-        PolicyScrap policyScrap = policyService.createPolicyScrap(policyScrapDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(policyScrap);
+    public ResponseEntity<PolicyScrapDTO> createPolicyScrap(@RequestBody PolicyScrapDTO policyScrapDTO) throws Exception {
+        policyService.createPolicyScrap(policyScrapDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(policyScrapDTO);
     }
 
     @GetMapping("/policies/scrap/{memberId}")
-    public ResponseEntity<List<Policy>> getMyPolicyScraps(@PathVariable Long memberId) throws Exception {
-        return ResponseEntity.ok(policyService.getMyPolicyScraps(memberId));
+    public ResponseEntity<Page<PolicySummaryDTO>> getMyPolicyScraps(@PathVariable Long memberId, @RequestParam int pageIndex) throws Exception {
+        return ResponseEntity.ok(policyService.getMyPolicyScraps(memberId, pageIndex));
     }
 
     @GetMapping("/policies/scrap")
