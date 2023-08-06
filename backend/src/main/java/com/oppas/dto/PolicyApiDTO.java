240188c095f847c9c2b9590a944695cf377dca5a
knavoid <k_shun@naver.com>
2023-08-06 09:41:24 +0900

    refactor: 파일명 변경

--- backend/src/main/java/com/oppas/dto/PolicyApiDTO.java
+++ backend/src/main/java/com/oppas/dto/PolicyApiDTO.java
@@ -3,47 +3,77 @@ package com.oppas.dto;
 import com.oppas.entity.policy.Policy;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Getter
-@Setter
 @AllArgsConstructor
-public class PolicyDTO {
+public class PolicyApiDTO {
+
     private final Integer id;
+
     private final Integer rnum;
+
     private final String bizId;
+
     private final String polyBizSjnm;
+
     private final String polyItcnCn;
+
     private final String sporCn;
+
     private final String sporScvl;
+
     private final String bizPrdCn;
+
     private final String prdRpttSecd;
+
     private final String rqutPrdCn;
+
     private final String ageInfo;
+
     private final String majrRqisCn;
+
     private final String empmSttsCn;
+
     private final String splzRlmRqisCn;
+
     private final String accrRqisCn;
+
     private final String prcpCn;
+
     private final String aditRscn;
+
     private final String prcpLmttTrgtCn;
+
     private final String rqutProcCn;
+
     private final String pstnPaprCn;
+
     private final String jdgnPresCn;
+
     private final String rqutUrla;
+
     private final String rfcSiteUrla1;
+
     private final String rfcSiteUrla2;
+
     private final String mngtMson;
+
     private final String mngtMrofCherCn;
+
     private final String cherCtpcCn;
+
     private final String cnsgNmor;
+
     private final String tintCherCn;
+
     private final String tintCherCtpcCn;
+
     private final String etct;
+
     private final String polyRlmCd;
 
     public Policy createPolicy(ModelMapper modelMapper) {
