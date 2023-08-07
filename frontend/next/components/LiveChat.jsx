0cf87082b082b5e3dbf40308d6c5be1d972ebea9
Inbeom Kim <dlsqja970210@naver.com>
2023-08-08 01:26:46 +0900

    feat: update websocket

--- frontend/next/components/LiveChat.jsx
+++ frontend/next/components/LiveChat.jsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useRef, useState, useEffect } from "react";
 import useStompClient from "./hooks/useStompClient";
+import axios from "axios";
 
 export default function LiveChat(props) {
-  const { client, messages } = useStompClient(
+  const { client, messages, setMessages } = useStompClient(
     "http://localhost:8081/api/policychat",
     `/sub/policychat${props.pageId}`
   );
@@ -13,20 +14,46 @@ export default function LiveChat(props) {
     if (client && client.connected) {
       const data = {
         message: messageContent,
-        memberId: "333",
-        nickName: "심경섭",
+        memberId: 13,
+        nickName: "zzankor",
         policyId: props.pageId,
       };
-
       client.publish({
         destination: "/pub/policychat",
         body: JSON.stringify(data),
       });
     }
+    console.log(typeof messages[0]);
   };
 
-  const onClick = () => {
-    return null;
+  const onClick = async () => {
+    try {
+      let data = {
+        message: null,
+        memberId: null,
+        nickName: null,
+        cursor: null,
+      };
+
+      if (messages && messages.length !== 0) {
+        const firstMessage = JSON.parse(messages[0]);
+        data = {
+          message: firstMessage.message,
+          memberId: firstMessage.memberId,
+          nickName: firstMessage.nickName,
+          cursor: firstMessage.cursor,
+        };
+      }
+
+      const response = await axios.post(
+        `http://localhost:8081/api/chats/${props.pageId}`,
+        data
+      );
+      console.log(response.data);
+      setMessages([response.data, ...messages]);
+    } catch (error) {
+      console.error("Error sending POST request:", error);
+    }
   };
 
   return (
@@ -38,10 +65,10 @@ export default function LiveChat(props) {
       </div>
       <ul>
         {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
+          <li key={index}>{JSON.parse(msg).message}</li>
         ))}
       </ul>
-      <button onClick={() => sendMessage("Hello from Next.js with STOMP!")}>
+      <button onClick={() => sendMessage("super sexy 심경섭!")}>
         Send Message
       </button>
     </div>
