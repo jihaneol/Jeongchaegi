2f89d7ab6300e0d6309f75316d5fd07ea7f77b4f
changkim1 <zzankor@naver.com>
2023-08-09 15:44:06 +0900

    feat: 백틱에 ${} 추가

--- frontend/next/components/Nav.jsx
+++ frontend/next/components/Nav.jsx
@@ -29,7 +29,7 @@ export default function Nav() {
           </Link>
 
           {/* 일단 기본값 1로 라우팅 */}
-          <Link href={`userData.isLogined ? /mypage/${nickname} : /login`}>
+          <Link href={`${userData.isLogined} ? /mypage/${nickname} : /login`}>
             <a className="text-2xl hover:text-blue-500">My Page</a>
           </Link>
         </div>
