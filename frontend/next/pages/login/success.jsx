79a40cef01c4b5420c8321a1a5e1bd5780b3ab2a
changkim1 <zzankor@naver.com>
2023-08-10 11:38:56 +0900

    feat: login success add spin

--- frontend/next/pages/login/success.jsx
+++ frontend/next/pages/login/success.jsx
@@ -4,6 +4,7 @@ import OurAxios from "../../config/ourAxios";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { userActions } from "../../store/user";
+import Spin from "../../components/Spin";
 
 export default function Success() {
   // useCookies 훅을 사용하여 쿠키 객체를 받아옵니다.
@@ -56,7 +57,7 @@ export default function Success() {
 	}, []);
 	
 
-  return (<div>
-		<button onClick={getLoginData}>요청!!</button>
+  return (<div className={style.success_wrapper}>
+		<Spin />
 	</div>);
 }
