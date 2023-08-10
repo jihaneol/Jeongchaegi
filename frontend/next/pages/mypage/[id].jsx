25dccd209629b4e3d3ae46f944bf6ab93932f80a
changkim1 <zzankor@naver.com>
2023-08-10 16:09:55 +0900

    feat: loading 시 spin

--- frontend/next/pages/mypage/[id].jsx
+++ frontend/next/pages/mypage/[id].jsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Nav from "../../components/Nav";
 import style from "../../styles/MyPage.module.css";
 import MyPageScrap from "../../components/MyPageScrap";
+import Spin from "../../components/Spin";
 
 export default function Page() {
   // 변수
@@ -76,7 +77,7 @@ export default function Page() {
             </div>
           </div>
           <div className={style.policyList_content}>
-            {!policyType ? (<MyPageScrap />) : ("")}
+            {!policyType ? (<MyPageScrap />) : (<Spin />)}
           </div>
         </div>
         <div className={style.followerList_wrapper}>
