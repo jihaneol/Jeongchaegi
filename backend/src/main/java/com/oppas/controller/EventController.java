49e96a71354521c71097b5e579f5920aa070d7fb
김성훈 <k_shun@naver.com>
2023-08-11 12:50:30 +0900

    feat: 일정 여부, 일정 수, 일정 지우기 기능

--- backend/src/main/java/com/oppas/controller/EventController.java
+++ backend/src/main/java/com/oppas/controller/EventController.java
@@ -17,18 +17,40 @@ public class EventController {
 
     private final EventService eventService;
 
+    @GetMapping("/possible/policies/{policyId}")
+    public ResponseEntity<Boolean> isPossibleEvent(@PathVariable Long policyId) throws Exception {
+        return ResponseEntity.ok(eventService.isPossibleEvent(policyId));
+    }
+
+    @GetMapping("/check/policies/{policyId}")
+    public ResponseEntity<String> checkEvent(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                              @PathVariable Long policyId) throws Exception {
+        Long memberId = principalDetails.getId();
+        return ResponseEntity.ok(eventService.checkEvent(memberId, policyId));
+    }
+
+    @GetMapping("/count/policies/{policyId}")
+    public ResponseEntity<Long> countMyEvents(@PathVariable Long policyId) throws Exception {
+        return ResponseEntity.ok(eventService.countMyEvents(policyId));
+    }
+
     @GetMapping("/form/policies/{policyId}")
     public ResponseEntity<EventDTO> getEventCreateForm(@PathVariable Long policyId) throws Exception {
         EventDTO event = eventService.getEventCreateForm(policyId);
         return ResponseEntity.ok(event);
     }
 
-    @PostMapping("/save/policies/{policyId}")
+    @PostMapping("/{eventId}/save/policies/{policyId}")
     public ResponseEntity<String> saveEvent(@AuthenticationPrincipal PrincipalDetails principalDetails,
-                                            @PathVariable Long policyId, @RequestParam String eventId) throws Exception {
+                                            @PathVariable String eventId, @PathVariable Long policyId) throws Exception {
         Member member = principalDetails.getMember();
         eventService.saveEvent(eventId, member, policyId);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventId);
+    }
+
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<String> deleteEvent(@PathVariable String eventId) throws Exception {
+        return ResponseEntity.ok(eventService.deleteEvent(eventId));
     }
 
 }
--- backend/src/main/java/com/oppas/service/EventService.java
+++ backend/src/main/java/com/oppas/service/EventService.java
@@ -7,7 +7,6 @@ import com.oppas.entity.event.Event;
 import com.oppas.entity.policy.Policy;
 import com.oppas.entity.policy.PolicyDate;
 import com.oppas.repository.EventRepository;
-import com.oppas.repository.MemberRepository;
 import com.oppas.repository.policy.PolicyDateRepository;
 import com.oppas.repository.policy.PolicyRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,9 +22,31 @@ import javax.persistence.EntityNotFoundException;
 public class EventService {
 
     private final EventRepository eventRepository;
-    private final MemberRepository memberRepository;
     private final PolicyRepository policyRepository;
     private final PolicyDateRepository policyDateRepository;
+
+    /**
+     * 해당 정책에 대해 일정 등록 가능 여부 파악
+     */
+    public Boolean isPossibleEvent(Long policyId) throws Exception {
+        Optional<PolicyDate> policyDate = policyDateRepository.findByPolicyId(policyId);
+        return policyDate.isPresent();
+    }
+
+    /**
+     * 정책에 대한 일정 가져오기
+     */
+    public String checkEvent(Long memberId, Long policyId) throws Exception {
+        Optional<Event> event = eventRepository.findByMemberIdAndPolicyId(memberId, policyId);
+        return event.map(Event::getId).orElse(null);
+    }
+
+    /**
+     * 해당 정책 일정을 등록한 사용자의 수
+     */
+    public Long countMyEvents(Long policyId) throws Exception {
+        return eventRepository.countByPolicyId(policyId);
+    }
 
     /**
      * 정책 정보를 통해 일정 생성 폼 반환
@@ -52,4 +74,11 @@ public class EventService {
         eventRepository.save(event);
     }
 
+    /**
+     * 일정을 캘린더에서 지우기
+     */
+    public String deleteEvent(String eventId) throws Exception {
+        eventRepository.deleteById(eventId);
+        return eventId;
+    }
 }
