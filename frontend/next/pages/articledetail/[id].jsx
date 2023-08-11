3c5a4e9679e81aecd2b290a88d017bb33d4efa31
rlaehd12 <dnpffldxjs12@gmail.com>
2023-08-11 13:01:41 +0900

    style: 지피티님 만만세

--- frontend/next/pages/articledetail/[id].jsx
+++ frontend/next/pages/articledetail/[id].jsx
@@ -2,13 +2,14 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Nav from "../../components/Nav";
 
-import { remark } from 'remark';
-import html from 'remark-html';
+import { remark } from "remark";
+import html from "remark-html";
 import axios from "axios";
 
 import { useDispatch, useSelector } from "react-redux";
+import style from "../../styles/ArticleDetail.module.css";
 
-// 잠깐 테스트용, 나중에 가능하면 ssr로 바꿀거임
+// 잠깐 테스트용, 나중에 가능하면 ssr로 바꿀거임========================================================
 
 // 일단 react-md 라이브러리 프리뷰는 주석처리, nextjs에서 말하는 renark라이브러리 써볼거임
 // const EditerMarkdown = dynamic(
@@ -28,7 +29,6 @@ import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 import OurAxios from "../../config/ourAxios";
 
-
 const EditerMarkdown = dynamic(
   () =>
     import("@uiw/react-md-editor").then((mod) => {
@@ -36,58 +36,59 @@ const EditerMarkdown = dynamic(
     }),
   { ssr: false }
 );
-// 테스트용 마지막줄
+// 테스트용 마지막줄 ======================================================================================
 
 export default function Page({ params }) {
-  const router = useRouter()
-  const [detailData, setDetailData] = useState(null)
-  const userData = useSelector(state => state.user);
+  const router = useRouter();
+  const [detailData, setDetailData] = useState(null);
+  const userData = useSelector((state) => state.user);
   const api = OurAxios();
   console.log(params);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios({
-      method:'get',
-      url:`http://3.36.131.236/api/posts/${router.query.id}`,
+      method: "get",
+      url: `http://3.36.131.236/api/posts/${router.query.id}`,
     })
-    .then((res)=>{
-      console.log(res);
-      setDetailData(res.data)
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-    .finally(()=>{
-      console.log('end detail data!');
-    })
-  }, [])
+      .then((res) => {
+        console.log(res);
+        setDetailData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("end detail data!");
+      });
+  }, []);
 
   // 수정, 삭제 버튼 함수
 
   function updateArticle() {
-    if (localStorage.getItem("userID") == detailData.memberId){
-      router.push(`/articleupdate?id=${router.query.id}`)
-    }
-    else{
-      alert('수정할수 없습니다!!!!!!!!!!')
+    // 다음 같은 사용자가 요청하는가
+    if (localStorage.getItem("userID") == detailData.memberId) {
+      router.push(`/articleupdate?id=${router.query.id}`);
+    } else {
+      alert("수정할수 없습니다!!!!!!!!!!");
     }
   }
   function deleteArticle() {
+    // 사용자가 같으면 삭제 가능, 다르면 삭제 불가
     console.log(typeof localStorage.getItem("userID"));
     if (localStorage.getItem("userID") == detailData.memberId) {
-      api.delete(`/posts/${router.query.id}`)
-      .then((res)=>{
-        console.log(res);
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
-      .finally(()=>{
-        router.push('/articlelist')
-      })
-    }
-    else{
-      alert('삭제할수 없습니다!!!')
+      api
+        .delete(`/posts/${router.query.id}`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          router.push("/articlelist");
+        });
+    } else {
+      alert("삭제할수 없습니다!!!");
     }
   }
 
@@ -96,20 +97,36 @@ export default function Page({ params }) {
   return (
     <>
       <Nav />
-      <div style={{ marginTop: "120px" }}>{router.query.id}</div>
-      {detailData ? 
-      <>
-        <div>title : {detailData.title} </div>
-        <EditerMarkdown source={detailData.content} />
-      </> : false}
-      {/* {userData.isLogined ? <>
-        <button onClick={updateArticle}>수정</button>
-        <br />
-        <button onClick={deleteArticle}>삭제</button>
-      </> : false} */}
-      <button onClick={updateArticle}>수정</button>
-      <br />
-      <button onClick={deleteArticle}>삭제</button>
+      <div className="flex flex-col items-center p-4 space-y-6">
+
+        {detailData ? (
+          <div className="w-full max-w-4xl bg-white shadow-md p-6 rounded-lg space-y-4 mt-24">
+            <h1 className="text-2xl font-semibold">{detailData.title}</h1>
+            <div className="p-4 bg-gray-100 rounded">
+              <EditerMarkdown source={detailData.content} />
+            </div>
+          </div>
+        ) : (
+          <div className="text-center py-4 text-gray-400">Loading...</div>
+        )}
+
+        {userData.isLogined && (
+          <div className="space-x-4">
+            <button
+              onClick={updateArticle}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
+            >
+              수정
+            </button>
+            <button
+              onClick={deleteArticle}
+              className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-200"
+            >
+              삭제
+            </button>
+          </div>
+        )}
+      </div>
     </>
-    );
+  );
 }
