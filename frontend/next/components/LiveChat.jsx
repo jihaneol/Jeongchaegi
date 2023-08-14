1c3357e2e8218c194918e9223306039a5faf0fed
rlaehd12 <dnpffldxjs12@gmail.com>
2023-08-14 12:29:48 +0900

    fix: 경로 추가 수정

--- frontend/next/components/LiveChat.jsx
+++ frontend/next/components/LiveChat.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function LiveChat(props) {
   const [inputMessage, setInputMessage] = useState("");
   const { client, messages, setMessages } = useStompClient(
-    "http://3.36.131.236/ws/policychat",
+    "http://www.jeongchaegi.com/ws/policychat",  
     `/sub/policychat${props.pageId}`
   );
   // "http://3.36.131.236/ws/policychat", 서버
@@ -81,7 +81,7 @@ export default function LiveChat(props) {
 
       console.log(data);
       const response = await axios.post(
-        `http://3.36.131.236/api/chats/${props.pageId}`,
+        `http://www.jeongchaegi.com/api/chats/${props.pageId}`,
         data
       );
       console.log(response.data); // 객체 배열
