2bff1e2d32f8fdd5118b9f0a8f8acf317c85d77e
changkim1 <zzankor@naver.com>
2023-08-16 14:55:09 +0900

    feat: 등록 불가능한 정책일 때 popover 띄우기

--- frontend/next/components/CannotRegistNotice.jsx
+++ frontend/next/components/CannotRegistNotice.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import { FaRegBell } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import style from "../styles/PolicyDetail.module.css";
 
 export default function CannotNoticeRegister() {
-	return (
-		<div>CannotNoticeRegister</div>
-	)
+  const userData = useSelector((state) => state.user);
+  const [open, setOpen] = useState();
+
+  function mouseEnter() {
+    setOpen(true);
+    console.log("enter");
+  }
+
+  function mouseLeave() {
+    setOpen(false);
+    console.log("leave");
+  }
+
+  return (
+    <div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+      <FaRegBell />
+      <div className={`${style.popover_box} ${open ? style.visible : ''}`}>
+        <div className={style.popover_header}>주의!</div>
+        <div className={style.popover_content}>
+          {userData.isLogined
+            ? `등록할 수 없는 정책입니다`
+            : `로그인이 필요한 서비스입니다`}
+        </div>
+      </div>
+    </div>
+  );
 }
