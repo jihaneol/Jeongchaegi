e5ab6440373e7680dbbf00259955025715f18255
changkim1 <zzankor@naver.com>
2023-08-16 09:47:12 +0900

    test: 일정폼 얻기 로그 찍어보기

--- frontend/next/components/NoticeModal.jsx
+++ frontend/next/components/NoticeModal.jsx
@@ -31,16 +31,22 @@ export default function NoticeModal({ type, title, modalClose, setRefreshFlag, e
 		}).then((res) => {
 			console.log("캘린더 목록 가져오기 성공!");
 			console.log(res);
+			// 캘린더 이름이 "정채기"인 거 찾기
 			flag = findJCG(res.data.calendars);
 		}).catch((err) => {
 			console.log("캘린더 목록 가져오기 실패");
 			console.log(err);
 		});
-		// 캘린더 이름이 "정채기"인 거 찾기
 		console.log("정채기인 거 찾았어?");
 		console.log(flag);
 		// 정책 아이디(listIdProp)로 일정 생성폼 가져오기 -> 이벤트 폼 얻기
-		// await api.get(`/api/events/form/policies/${listIdProp}/`)
+		await api.get(`/api/events/form/policies/${listIdProp}/`).then((res) => {
+			console.log("생성폼 얻기 성공!");
+			console.log(res);
+		}).catch((err) => {
+			console.log("생성폼 얻기 실패!");
+			console.log(err);
+		});
 		// 캘린더 아이디 + 이벤트 폼 으로 일정생성 -> 일정 아이디 발급 ([0]: 시작일, [1]: 마감일 -> 2번 요청보내야함)
 		// 일정 아이디와 정책 아이디로 일정 저장 -> 위에서 뱉어낸 eventID 로 마찬가지로 2번 보내야함.
 		alert("성공적으로 등록되었습니다.");
