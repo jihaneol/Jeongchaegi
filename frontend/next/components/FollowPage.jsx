fd872a76174618055145256de58d62274089fa72
Inbeom Kim <dlsqja970210@naver.com>
2023-08-17 11:03:07 +0900

    fix: OurAxios import 해결

--- frontend/next/components/FollowPage.jsx
+++ frontend/next/components/FollowPage.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import ourAxios from "../config/ourAxios";
+import OurAxios from "../../config/ourAxios";
 
 export default function FollowPage(props) {
   const user = props.user;
   const [isFollow, setIsFollow] = useState(true);
   const router = useRouter();
-  const api = ourAxios();
+  const api = OurAxios();
 
   useEffect(() => {
     // 팔로우 여부 확인 (게시판에서 접근했을 때)
--- frontend/next/pages/follow/[userID].jsx
+++ frontend/next/pages/follow/[userID].jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import Nav from "../../components/Nav";
 import Image from "next/image";
 import FollowPage from "../../components/FollowPage";
-import ourAxios from "../config/ourAxios";
+import OurAxios from "../../config/ourAxios";
 
 export default function Follow() {
-  const api = ourAxios();
+  const api = OurAxios();
   const [search, setSearch] = useState(""); // 검색어
   const [followNum, setFollowNum] = useState(0); // 팔로우 수
   const [followList, setFollowList] = useState([]); // 팔로우 리스트
